f92ee108cda9d12d2ccda46d1ea004b25934f680
Julian Peña <julian.pena-m@mail.escuelaing.edu.co>
2021-11-29 20:39:42 -0500

    Implementando NecesidadBean

--- src/main/java/edu/eci/cvds/samples/services/impl/NecesidadServiceIMPL.java
+++ src/main/java/edu/eci/cvds/samples/services/impl/NecesidadServiceIMPL.java
@@ -3,16 +3,20 @@ package edu.eci.cvds.samples.services.impl;
 import edu.eci.cvds.samples.entities.Necesidad;
 import edu.eci.cvds.samples.persistence.NecesidadDAO;
 import edu.eci.cvds.samples.services.NecesidadService;
+import com.google.inject.Inject;
 
 import java.util.List;
 
 public class NecesidadServiceIMPL implements NecesidadService {
+    @Inject
     private NecesidadDAO DAOnec;
 
+    @Override
     public List<Necesidad> consultarNecesidad(String nombre){
         return DAOnec.consultarNecesidad(nombre);
 
     }
+    @Override
     public void registrarNecesidad(Necesidad necesidad){
         DAOnec.registrarNecesidad(necesidad);
     }
