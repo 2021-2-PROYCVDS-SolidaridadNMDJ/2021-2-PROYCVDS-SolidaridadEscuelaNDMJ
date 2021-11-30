d4cc186bfb3c66f0061c9f55a934458c11cb1222
Julian Peña <julian.pena-m@mail.escuelaing.edu.co>
2021-11-29 20:23:12 -0500

    Implementando NecesidadBean

--- src/main/java/edu/eci/cvds/samples/beans/NecesidadBean.java
+++ src/main/java/edu/eci/cvds/samples/beans/NecesidadBean.java
@@ -11,7 +11,7 @@ import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
-import java.sql.Date;
+//import java.sql.Date;
 import java.util.Date;
 
 @SuppressWarnings("deprecation")
