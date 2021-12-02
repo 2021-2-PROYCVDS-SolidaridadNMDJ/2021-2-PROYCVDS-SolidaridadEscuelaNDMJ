8d9599e43864040081150e6bdc4e71d902df96da
Julian Peña <julian.pena-m@mail.escuelaing.edu.co>
2021-12-01 22:49:43 -0500

    Registro Necesidad

--- src/main/java/edu/eci/cvds/samples/beans/NecesidadBean.java
+++ src/main/java/edu/eci/cvds/samples/beans/NecesidadBean.java
@@ -22,9 +22,9 @@ public class NecesidadBean extends BaseBean {
      private NecesidadService necesidadService;
      private String message;
 
-     public void registrarNecesidad(String id_categoria,String nombre,String descripcion,String urgencia)throws SolidaridadException{
+     public void registrarNecesidad(int id, String id_categoria,String nombre,String descripcion,String urgencia)throws SolidaridadException{
          try {
-            Necesidad necesidad = new Necesidad(id_categoria, nombre, descripcion, urgencia, new Date((new java.util.Date()).getTime()),"En Proceso", new Date((new java.util.Date()).getTime()));
+            Necesidad necesidad = new Necesidad(id, id_categoria, nombre, descripcion, urgencia, new Date((new java.util.Date()).getTime()),"En Proceso", new Date((new java.util.Date()).getTime()));
             necesidadService.registrarNecesidad(necesidad);
             this.message = "La necesidad se registro correctamente";
         } catch (Exception e){
